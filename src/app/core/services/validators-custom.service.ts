83aa1814d9efb793c87db97648f66478734451c9
Joel Estumano <joelestumano@gmail.com>
2022-03-09 19:29:34 -0300

    hot fix validator

--- src/app/core/services/validators-custom.service.ts
+++ src/app/core/services/validators-custom.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, FormArray, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
-/* import { AlertService } from './alert.service'; */
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidatorsCustom {
-
-  /* constructor(private readonly alertService: AlertService) { } */
 
   static allowsToRedeem(saldoAcumulado: number): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
@@ -22,15 +19,30 @@ export class ValidatorsCustom {
     }
   }
 
+  public applyInvalidCustomClass(form: FormGroup, field: string, classCustom: string) {
+    const classe = '{"' + classCustom + '": ' + this.virifyValidTouched(form, field) + '}'
+    console.log(classe)
+    return JSON.parse(classe)
+  }
+
+  public applyInvalidClass(form: FormGroup, field: string) {
+    return {
+      'is-invalid': this.virifyValidTouched(form, field),
+      'has-feedback': this.virifyValidTouched(form, field)
+    }
+  }
+
+  public virifyValidTouched(form: FormGroup | any, field: string) {
+    return !form.get(field).valid && (form.get(field).touched || form.get(field).dirty);
+  }
+
   public applyValidationCustomClass(index: number, form: any) {
     if (!form.at(index)) {
       throw new Error('There is no formControl in position: ' + index);
     }
     return {
       'is-invalid': this.isFormArrayFieldValid(index, form),
-      'has-valid': !this.isFormArrayFieldValid(index, form),
-      /*  */
-      /* 'btn-outline-danger':  this.isFormArrayFieldValid(index, form), */
+      'has-valid': !this.isFormArrayFieldValid(index, form)
     }
   }
 
